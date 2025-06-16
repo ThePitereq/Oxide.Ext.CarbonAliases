799f3dfcd916e3f0e44bdc9f8682207b82905f88
hizen <134218727+hizenxyz@users.noreply.github.com>
2025-06-16 02:11:11 -0400

    BaseModule to return default if module not loaded ImageDatabaseModule to do some null checks

--- ImageDatabaseModule.cs
+++ ImageDatabaseModule.cs
@@ -10,16 +10,16 @@ namespace Oxide.Ext.CarbonAliases
     {
         private static Plugin ImageLibrary;
 
-        public ImageDatabaseModule() {
-            ImageLibrary = Interface.GetMod().RootPluginManager.GetPlugin("ImageLibrary");
-            if (ImageLibrary == null)
-                Interface.Oxide.LogWarning("ImageLibrary not found! UI building will print errors!");
+        public ImageDatabaseModule(Plugin plugin = null) {
+            ImageLibrary = plugin;
         }
 
         public void QueueBatch(bool @override, IEnumerable<string> urls) => QueueBatch(@override, null, urls);
         
         public void QueueBatch(bool @override, Action<List<ImageQueueResult>> onComplete, IEnumerable<string> urls)
         {
+            if (ImageLibrary == null) return;
+
             Dictionary<string, string> images = Pool.Get<Dictionary<string, string>>();
             images.Clear();
             foreach (string url in urls)
@@ -38,17 +38,19 @@ namespace Oxide.Ext.CarbonAliases
 
         public void Queue(Dictionary<string, string> urls)
         {
+            if (ImageLibrary == null) return;
+
             foreach (var kv in urls)
                 ImageLibrary.Call<bool>("AddImage", kv.Value, kv.Key, 0uL);
         }
         public void DeleteImage(string url)
         {
-            ImageLibrary.Call("RemoveImage", url);
+            ImageLibrary?.Call("RemoveImage", url);
         }
         
         public void AddMap(string key, string url)
         {
-            ImageLibrary.Call<bool>("AddImage", url, key, 0uL);
+            ImageLibrary?.Call<bool>("AddImage", url, key, 0uL);
         }
         public void RemoveMap(string key, string url)
         {
@@ -57,17 +59,24 @@ namespace Oxide.Ext.CarbonAliases
         
         public bool HasImage(string key)
         {
+            if (ImageLibrary == null) return false;
+
             return ImageLibrary.Call<bool>("HasImage", key, 0UL);
         }
         
         public uint GetImage(string key, float scale = 0, bool silent = false)
         {
+            if (ImageLibrary == null) return 0;
+            
             return Convert.ToUInt32(ImageLibrary.Call<string>("GetImage", key));
         }
         public string GetImageString(string key, float scale = 0, bool silent = false)
         {
+            if (ImageLibrary == null) return null;
+
             return ImageLibrary.Call<string>("GetImage", key);
         }
+
         public void DeleteImage(string url, float scale = 0)
         {
 
